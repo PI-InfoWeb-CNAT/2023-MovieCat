2307956225544b2c117a38308f43b4b6bc53d6dd
Maria Cândida Xavier <xavier.m@academico.ifrn.edu.br>
2023-07-04 15:58:27 -0300

    crud funcionarios

---
+++ source/AgendMovies/AgendMovies/Banco_de_Dados/Tabelas.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace AgendMovies.Modelos
+{
+    public class Tabelas: DbContext
+    {
+        public Tabelas() : base("Asp_Net_MVC_CS") { }
+        public DbSet<Funcionario> Funcionario { get; set; }
+
+
+    }
+}
---
+++ source/AgendMovies/AgendMovies/Modelos/Funcionario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AgendMovies.Modelos
+{
+    public class Funcionario
+    {
+        public string nome { get; set; }
+
+        public int cpf { get; set; }
+
+        public int data { get; set; }
+
+        public int telefone { get; set; }
+
+        public string email { get; set; }
+
+        public string senha { get; set; }
+
+    }
+}
