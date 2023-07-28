48e9a77082cc04cee12218383e78ccdd5f894ee9
Maria Cândida Xavier <xavier.m@academico.ifrn.edu.br>
2023-07-28 17:57:34 -0300

    iniciando CRUD de filmes

---
+++ source/AgendMovies/AgendMovies/Controllers/CRUDFilmesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AgendMovies.Modelos;
+
+namespace AgendMovies.Controllers
+{
+    public class CRUDFilmesController : Controller
+    {
+        Tabelas context = new Tabelas();
+        // GET: CRUDFilmes
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
