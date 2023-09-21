4aca7d5a2f2727b93458f7dd4b1aaa68be250031
Maria Cândida Xavier <xavier.m@academico.ifrn.edu.br>
2023-09-21 20:53:19 -0300

    tentando direcionar filme pra tela inicial

--- source/AgendMovies/AgendMovies/Controllers/HomeController.cs
+++ source/AgendMovies/AgendMovies/Controllers/HomeController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AgendMovies.Modelos;
 
 namespace AgendMovies.Controllers
 {
     public class HomeController : Controller
     {
+        Tabelas BD = new Tabelas();
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+           
+            return View(BD.Filmes.OrderBy(c => c.FilmeId).ToList());
         }
     }
 }
--- source/AgendMovies/AgendMovies/Modelos/Filme.cs
+++ source/AgendMovies/AgendMovies/Modelos/Filme.cs
@@ -10,15 +10,15 @@ namespace AgendMovies.Modelos
     {
         
         public long FilmeId { get; set; }
-        [Required(ErrorMessage = "Por favor, insira o nome do filme.")]
+        //[Required(ErrorMessage = "Por favor, insira o nome do filme.")]
         public string Nome { get; set; }
 
-        [StringLength(300, ErrorMessage = "Sua sinopse ultrapassou o limite de caracteres ou não alcançou o limite mínimo!", MinimumLength = 30)]
-        [Required(ErrorMessage = "Por favor, escreva a sinopse do filme.")]
+        //[StringLength(300, ErrorMessage = "Sua sinopse ultrapassou o limite de caracteres ou não alcançou o limite mínimo!", MinimumLength = 30)]
+        //[Required(ErrorMessage = "Por favor, escreva a sinopse do filme.")]
         public string Sinopse {get; set;}
 
         public string Indicativa { get; set; }
-        [Required(ErrorMessage = "Por favor, insira um gênero para o filme.")]
+        //[Required(ErrorMessage = "Por favor, insira um gênero para o filme.")]
         public string Genero { get; set; }
         public byte[] ConteudoDaFoto { get; set; }
         public string TipoDaFoto { get; set; }
