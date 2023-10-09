5f8ea296b771864302719651eab802e76092eff2
Maria Cândida Xavier <xavier.m@academico.ifrn.edu.br>
2023-10-09 16:07:23 -0300

    cadastrarcliente

--- source/AgendMovies/AgendMovies/Controllers/ClienteController.cs
+++ source/AgendMovies/AgendMovies/Controllers/ClienteController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AgendMovies.Modelos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,42 @@ namespace AgendMovies.Controllers
 {
     public class ClienteController : Controller
     {
+        Tabelas Banco = new Tabelas();
         // GET: Cliente
-        public ActionResult Index()
+        public ActionResult Cadastrar()
         {
-            return View();
+            return View(new Cliente());
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Cadastrar(Cliente f, HttpPostedFileBase arquivo)
+        {
+
+            if (arquivo != null)
+            {
+                f.TipoDaFoto = arquivo.ContentType;
+                f.Foto = SetLogoTipo(arquivo);
+            }
+            Banco.Funcionarios.Add(f);
+            Banco.SaveChanges();
+            return RedirectToAction("Visualizar", "Funcionarios");
+
+        }
+        private byte[] SetLogoTipo(HttpPostedFileBase arquivo)
+        {
+            var bytesFile = new byte[arquivo.ContentLength];
+            arquivo.InputStream.Read(bytesFile, 0, arquivo.ContentLength);
+            return bytesFile;
+        }
+        public FileContentResult GetLogoTipo(int? id)
+        {
+
+            Funcionario F = Banco.Funcionarios.Find(id);
+            if (F != null)
+            {
+                return File(F.Foto, F.TipoDaFoto);
+            }
+            return null;
         }
     }
 }
--- source/AgendMovies/AgendMovies/Modelos/Cliente.cs
+++ source/AgendMovies/AgendMovies/Modelos/Cliente.cs
@@ -8,6 +8,8 @@ namespace AgendMovies.Modelos
 {
     public class Cliente
     {
+        public long Id { get; set; }
+
         [Required(ErrorMessage = "Insira seu nome!")]
         public string Nome { get; set; }
         
@@ -17,7 +19,10 @@ namespace AgendMovies.Modelos
         [Required(ErrorMessage = "Por favor, insira um endereço de email válido.")]
         public string Email { get; set; }
 
-        
+        [StringLength(30, ErrorMessage = "Sua senha deve ter no mínimo 5 caracteres!", MinimumLength = 5)]
+        [Required(ErrorMessage = "Por favor, escreva a senha.")]
         public string Senha { get; set; }
+        public byte[] Foto { get; set; }
+        public string TipoDaFoto { get; set; }
     }
 }
